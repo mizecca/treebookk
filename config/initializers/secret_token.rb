cabe067e39920113276ea7c45f05de7902d64362
OpenShift guest <mike.w.wallace@gmail.com>
2013-05-18 16:26:27 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = 'df528b75747894062a2578fcfa6932ddc007937261ca6774b83dfbd103b1a609aad13f8d649589f7c90535cdb7f9ea86ee0701942dfe45433dc802ac7c13fb8e'
